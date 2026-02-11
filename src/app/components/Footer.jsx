b8ff1a20b3a5cf917716958adb3f1757c6123c15
Mouhsine93 <msowavy@gmail.com>
2026-02-11 02:54:25 +0100

    footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="relative border-t border-t-[#33353F] text-white h-24">
       <div className="container mx-auto relative h-20">
         <p className="absolute bottom-0 right-0 mb-4 text-slate-600">
-          © 2026 Portfolio - Mouhsine
+          © 2026
         </p>
       </div>
     </footer>
