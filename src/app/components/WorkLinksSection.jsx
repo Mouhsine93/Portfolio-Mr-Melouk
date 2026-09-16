8f76d354a1995b67c5ff62cddb989de13aa92edd
Mouhsine93 <msowavy@gmail.com>
2026-09-16 19:41:11 +0200

    fix(ui): retire les icones GitHub/GitLab, garde du texte souligne
    Redondant avec le texte qui nomme deja GitHub/GitLab, et incoherence
    de taille entre les deux icones. Coherent avec la simplification
    minimaliste du reste du site.

--- src/app/components/WorkLinksSection.jsx
+++ src/app/components/WorkLinksSection.jsx
@@ -1,9 +1,6 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
-import GithubIcon from "../../../public/github-icon.svg";
-import GitlabIcon from "../../../public/gitlab-icon.svg";
 
 const WorkLinksSection = () => {
   return (
@@ -11,12 +8,12 @@ const WorkLinksSection = () => {
       <p className="mb-2">Voici une sélection de mes projets.</p>
       <div className="flex flex-wrap items-center justify-center gap-2">
         <span>Retrouvez l&apos;ensemble de mes travaux sur mon</span>
-        <Link href="https://github.com/Mouhsine93" className="inline-flex items-center gap-1 text-white hover:text-indigo-300">
-          <Image src={GithubIcon} alt="Github Icon" width={22} height={22} /> GitHub
+        <Link href="https://github.com/Mouhsine93" className="text-white hover:text-indigo-300 underline">
+          GitHub
         </Link>
         <span>&amp;</span>
-        <Link href="https://gitlab.com/Mouhsine93" className="inline-flex items-center gap-1 text-white hover:text-indigo-300">
-          GitLab <Image src={GitlabIcon} alt="Gitlab Icon" width={26} height={24} />
+        <Link href="https://gitlab.com/Mouhsine93" className="text-white hover:text-indigo-300 underline">
+          GitLab
         </Link>
       </div>
     </div>
